0ecad46c25b942683e4168e992ae30e7c6312356
Gibsondignity <gibsondignty@gmail.com>
2025-09-10 23:22:04 +0000

    dashboard urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('bookings/', views.manage_bookings, name='manage_bookings'),
     path('doctors/', views.manage_doctors, name='manage_doctors'),
     path('hospitals/', views.manage_hospitals, name='manage_hospitals'),
+    path('services/', views.manage_services, name='manage_services'),
+    path('users/', views.manage_users, name='manage_users'),
     path('appointments/', views.view_appointments, name='view_appointments'),
 ]
