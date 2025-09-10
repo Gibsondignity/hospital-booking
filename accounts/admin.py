d1f80831cba09d50fab206e08c2460f764601048
Gibsondignity <gibsondignty@gmail.com>
2025-09-10 22:38:06 +0000

    admin account

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+admin.site.site_header = "Hospital Management Admin"
+admin.site.site_title = "Hospital Management Admin Portal"
+admin.site.index_title = "Welcome to Hospital Management Admin Portal"
+admin.site.register(CustomUser)
